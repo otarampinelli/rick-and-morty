4afdd1e89685161fee5e12912ee00c73ff9cc1ce
otarampinelli <otaviorampinellielias@gmail.com>
2022-05-19 15:33:15 -0300

    fix: created folder to a group app file and app style

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Navbar from '../Navbar/Navbar'
+import CharacterList from '../CharacterList/CharacterList';
+
+import './App.css'
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+            <CharacterList />
+        </div>
+    )
+}
+
+export default App;
