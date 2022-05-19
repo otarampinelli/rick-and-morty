e5e7452792c15546b4ba1c89c4a9b3150c2445c0
otarampinelli <otaviorampinellielias@gmail.com>
2022-05-19 15:34:18 -0300

    feature: created navbar component and navbar style

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <section className='navbar-menu'>
+            <ul>
+                <li>
+                    Rick and Morty
+                </li>
+            </ul>
+        </section>
+    )
+}
+
+export default Navbar;
